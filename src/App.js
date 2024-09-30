84c87ab8ca4e0d1ddd91859b9d6a70dea2a06b0f
Paulo Souza <paulo.souza@kuehne-nagel.com>
2024-09-30 17:19:38 -0300

    delete button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 
 import {useState, useEffect} from "react";
 
+import { useFetch } from './hooks/useFetch';
+
 const url = "http://localhost:3000/products"
 
 function App() {
@@ -9,14 +11,22 @@ function App() {
   const [name,setName] = useState("");
   const [price,setPrice] = useState("");
 
-  useEffect( () => {
-    async function fetchData() {
-      const res = await fetch(url);
-      const data = await res.json();
-      setProducts(data);
-    }
-    fetchData();
-  }, []);
+  //customHook
+  const {data: items, httpConfig, loading, error} = useFetch(url);
+
+  //Remover produto
+  const handleRemove = (id) => {
+    httpConfig(id, "DELETE")
+  }
+
+  // useEffect( () => {
+  //   async function fetchData() {
+  //     const res = await fetch(url);
+  //     const data = await res.json();
+  //     setProducts(data);
+  //   }
+  //   fetchData();
+  // }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -25,17 +35,19 @@ function App() {
       name, price
     };
 
-    const res = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify(product),
-    });
+    // const res = await fetch(url, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json"
+    //   },
+    //   body: JSON.stringify(product),
+    // });
 
-    const addedProduct = await res.json();
+    // const addedProduct = await res.json();
 
-    setProducts((prevProducts) => [...prevProducts, addedProduct]);
+    // setProducts((prevProducts) => [...prevProducts, addedProduct]);
+
+    httpConfig(product, "POST");
 
     setName("");
     setPrice("");
@@ -44,14 +56,19 @@ function App() {
 
   return (
     <div className="App">
+      {/* Loading */}
+      {loading && <p>Carregando dados...</p>}
+      {error && <p>{error}</p>}
       <h1>Lista de produtos </h1>
+      {!loading && 
       <ul>
-        {products.map((product) => (
+        {items && items.map((product) => (
           <li key={product.id}>
             {product.name} - R$: {product.price}
+            <button onClick={() => handleRemove(product.id)}>Excluir</button>
           </li>
         ))}
-      </ul>
+      </ul>}
       <div className = "add-product">
         <form onSubmit={handleSubmit}>
           <label>
@@ -62,7 +79,9 @@ function App() {
             Preço:
             <input type='number' value={price} name="price" onChange={(e) => setPrice(e.target.value)}/>
           </label>
-          <input type='submit' value="Criar" />
+          {/* State de loading no post  */}
+          {loading && <input type='submit' disabled value="Aguarde" />}
+          {!loading && <input type='submit' value="Criar" />}
         </form>
       </div>
     </div>
